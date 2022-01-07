b5819fa4175c43bc71b978cb8a20dbeec062f177
Jason Berk <jason.r.berk@gmail.com>
2022-01-07 14:38:51 -0500

    setup custom fonts
    added link to google fonts
    https://fonts.google.com/share?selection.family=Montserrat%7COswald:wght@700
    
    applied typography styling
    
    dealing with font family, font weight and font sizes as independent styling in tailwind still confuses me :-\
    
    Not all fonts have all 10 of tailwind's provided weights.  In a case like this spec, we only downloaded one weight for each font family.  How do the [10 provided font weights](https://tailwindcss.com/docs/font-weight) get calculated....what do they actually change???
    
    Is it possible to setup custom weights per font family?????

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'body': ['Montserrat'],
+      'accent': ['Oswald'],
+    },
+    fontWeight: {
+      /*
+      remove all of the provided font weights as we only 
+      link to a single weight of each font.  Setting the 
+      font family implicitly sets the weight.
+
+      for the provided design, we'll just set `.font-body` on the <body>
+      and `.font-accent` where we need to override it.
+      */
+    },
     extend: {
       colors: {
         custom: {
